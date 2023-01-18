ee722a5ed28b328ffc0e504915c27b31e6158231
Michael Ohlrich <104468468+Ohl-Yeah@users.noreply.github.com>
2023-01-18 01:43:57 +0000

    Initial Commit for prints

---
+++ PrintTest.py
@@ -0,0 +1,21 @@
+#comment
+
+e = 'sweet'
+print(e)
+
+my_num = 1234
+
+print(e,my_num)
+
+# Examples of using f-strings 
+print(f'e + my_num')
+print(f'This is a {e} new way to add {my_num}')
+
+# To get the type back from a variables
+my_list = '1234'
+print(type(my_list))
+
+# Casting / convert
+my_cast = 1234
+my_cast = float(my_cast)
+print(my_cast)
